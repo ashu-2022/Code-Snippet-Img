9da07bd0cfab9dc71f8a18397d553f71cc0402ff
ashu-2022 <ashutoshpandit308@gmail.com>
2024-02-23 14:12:19 +0530

    Initial Project Setup

---
+++ components/CodeEditor.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Resizable } from "re-resizable";
+
+const CodeEditor = () => {
+  return (
+    <Resizable
+      minHeight={466}
+      minWidth={510}
+      maxWidth={1024}
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi et, dolor voluptatem repellendus, iusto officia assumenda ut est omnis ad adipisci cupiditate dolorem. Veritatis corrupti necessitatibus veniam soluta fugiat ea atque, optio tempore reiciendis aut inventore, neque odio ducimus obcaecati dicta. Enim nam optio est, autem velit illo reiciendis temporibus nihil. Optio ab quae accusantium atque placeat debitis maxime aperiam praesentium. Odit temporibus at, soluta beatae voluptate aliquam, officia consequatur mollitia dolores veniam, quisquam quo omnis nesciunt ratione! Explicabo atque natus illo iure magni distinctio, possimus dolorum doloremque, minima expedita, quisquam rem. Tenetur quos qui nostrum possimus veniam modi? Doloremque?
+    </Resizable>
+  )
+}
+
+export default CodeEditor
